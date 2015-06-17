28ed3e9f2a380b811ca726e3d7a3efd16b13f47e
Omkar Masurkar <omalphastreak@gmail.com>
2015-06-17 19:36:58 +0530

    Update USD.c

--- USD.c
+++ USD.c
@@ -2,6 +2,8 @@
 //My Institue Summer Technical Project
 //CodeForUDS
 int trigPin1= 13;
+int trigPin2= 3;
+int trigPin3= 4;
 
 int echoPin1 = 11;
 int echoPin2 = 12;
@@ -17,6 +19,8 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(trigPin1, OUTPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(trigPin3, OUTPUT);
   pinMode(echoPin1, INPUT);
   pinMode(echoPin2, INPUT);
   pinMode(echoPin3, INPUT);
@@ -37,12 +41,26 @@ void loop()
   distance1 = distance1/2.;
   duration1 = pulseIn(echoPin2, HIGH);
   
+   digitalWrite(trigPin2, LOW);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin2, LOW);
+  
   duration2 = pulseIn(echoPin2 , HIGH);
+  
   duration2 = (duration2)/10000.;
   distance2 = (duration2)*346.;
   distance2 = (distance2)/2.;
   
+   digitalWrite(trigPin3, LOW);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin3, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin3, LOW);
+  
   duration3 = pulseIn(echoPin3 , HIGH);
+  
   duration3 = (duration3)/10000.;
   distance3 = (duration3)*346.;
   distance3 = (distance3)/2.;
